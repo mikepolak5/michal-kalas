36237b49e473a871b6aec647c4bfecaa826af174
Michal Kalas <mikikalas@gmail.comgit config --global user.name mikepolak5>
2017-11-07 18:43:36 +0100

    calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-    System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.getAddResult(5, 2);
+        int substractResult = calculator.getSubstractResult(6, 3);
+
+        if(addResult == 7) {
+            System.out.println("Test ok!");
+        } else {
+            System.out.println("Error!");
+        }
+        if (substractResult == 3) {
+            System.out.println("Test ok!");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int getAddResult(int a, int b) {
+        return a + b;
+    }
+    public int getSubstractResult(int a, int b) {
+        return a - b;
+    }
+}
