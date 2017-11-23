b21ce1054a621285756011379621de06e750015e
Michal Kalas <mikikalas@gmail.comgit config --global user.name mikepolak5>
2017-11-23 12:26:53 +0100

    8.2

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,33 @@
+package com.kodilla.exception.test;
+
+import java.nio.file.Files;
+import java.util.stream.Stream;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException b) {
+            System.out.println("You can't divide by 0! " + b);
+        } finally {
+            System.out.println("Right away!");
+        }
+        }
+}
