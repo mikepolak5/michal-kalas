0e5e2e15ae4b1b82192bf49e4788001b2e00c247
Michal Kalas <mikikalas@gmail.comgit config --global user.name mikepolak5>
2018-02-26 23:37:15 +0100

    20.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/Watcher.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class Watcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Watcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("Processing order");
+    }
+}
